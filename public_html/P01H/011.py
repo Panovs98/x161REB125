036762742be314a5bde999fe572a101fa6feaa27
Aivis Panovs <x161REB125@sc.etf.rtu.lv>
2017-03-29 11:38:34 +0300

    public_html

---
+++ public_html/P01H/011.py
@@ -0,0 +1,5 @@
+for k in range (0,51):
+	
+    i=k*k;
+    print '<h2><font size="3" color="#660000"><br>%d %d</br></h2>' %((k),(i))
+
---
+++ public_html/skripts.py
@@ -0,0 +1,2 @@
+for k in range (1,10):
+    print "<h1>linija %d </h1>" % (k)
